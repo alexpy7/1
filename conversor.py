569082ef505ee2d45bed8537d7fbf9006fefc316
alexpy7 <alexsmx92@gmail.com>
2016-11-20 17:26:35 +0100

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,12 @@
+
+def convertir_a_kilometros(millas):
+    '''(Number)->number
+    convertir a kilomentros las millas mi loco
+    '''
+    kilometros = millas / 0.62137
+    print(millas,"millas","son",kilometros,"kilometros.")
+    
+def convertir_a_millas(kilometros):
+    millas = kilometros * 0.62137
+    print(kilometros,"kilometros","son",millas,"millas.")
+    
